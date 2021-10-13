e4f6f2f4fc32dcd947b1b7110c03f61ff8135d38
pkoi5088 <pkoi5088@gmail.com>
2021-10-13 22:11:19 +0900

    폴더 구조 정리 및 파일 정리

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# baekjoon
+# ProblemSolve
+백준, sw-expert, codeforce 등등 PS할 때 커밋하기
 ## 2021.06.17 시작
 하루에 하나는 아니더라도 문제 풀때마다 커밋하기
---
+++ SW-Expert/basic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	freopen("input.txt", "r", stdin);
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		/* 구현 */
+	}
+	return 0;
+}
