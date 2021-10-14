a937e8948868a300206f4df60347ab1898f7829c
pkoi5088 <pkoi5088@gmail.com>
2021-10-14 13:32:55 +0900

    2021-10-14

--- SW-Expert/cpp/12741.cpp
+++ SW-Expert/cpp/12741.cpp
@@ -10,11 +10,16 @@ int main(int argc, char** argv)
 	cin.tie(NULL); cout.tie(NULL);
 	int test_case;
 	int T;
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	cin >> T;
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		/* 구현 */
+		int A, B, C, D, ret = -1;
+		cin >> A >> B >> C >> D;
+		ret = min(B, D) - max(A, C);
+		if (ret < 0) ret = 0;
+		cout << '#' << test_case << ' ' << ret << endl;
 	}
 	return 0;
 }
