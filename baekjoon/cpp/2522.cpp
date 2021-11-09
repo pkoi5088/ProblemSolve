a84561a09687e84561493ef38eecb21240fb65ba
pkoi5088 <pkoi5088@gmail.com>
2021-11-09 15:37:21 +0900

    2021-11-09

---
+++ baekjoon/cpp/2522.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 0; j < N - i; ++j)
+			cout << ' ';
+		for (int j = 0; j < i; ++j)
+			cout << '*';
+		cout << endl;
+	}
+	for (int i = 1; i < N; ++i) {
+		for (int j = 0; j < i; ++j)
+			cout << ' ';
+		for (int j = 0; j < N - i; ++j)
+			cout << '*';
+		cout << endl;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	//freopen("input.txt", "r", stdin);
+	solve();
+	return 0;
+}
