32682a745b646166c29265f3cfb1ea986cd5c1fc
pkoi5088 <pkoi5088@gmail.com>
2021-11-21 13:39:18 +0900

    basic.cpp Update

---
+++ study/basic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	freopen("input.txt", "r", stdin);
+	solve();
+	return 0;
+}
