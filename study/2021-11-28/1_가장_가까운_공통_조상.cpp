3a3d786f41db9c23a1269187abfafba80c626770
pkoi5088 <pkoi5088@gmail.com>
2021-11-24 15:41:46 +0900

    2021-11-28

--- study/2021-11-28/1_가장_가까운_공통_조상.cpp
+++ study/2021-11-28/1_가장_가까운_공통_조상.cpp
@@ -1,46 +1,42 @@
 #include <iostream>
 #include <cstring>
-#define MAX_N 10000
-#define MAX_K 14
+#define MAX_N 10001
 #define endl '\n'
 
 using namespace std;
 
-void setLevel(int* level,int* parent,int idx){
-    if(level[idx]!=-1) return;
-
-    if(parent[idx]==-1) level[idx]=1;
-    else{
-        setLevel(level,parent,parent[idx]);
-        level[idx]=level[parent[idx]]+1;
-    }
-}
-
 void solve() {
-    int N,A,B,root;
-    int parent[MAX_N],dp[MAX_N][MAX_K],level[MAX_N];
-    memset(parent,-1,sizeof(parent));
-    memset(dp,-1,sizeof(dp));
-    memset(level,-1,sizeof(level));   
-    for(int i=0;i<N-1;++i){
-        cin>>A>>B;
-        parent[B-1]=A-1;
+    int N, A, B;
+    int parent[MAX_N], check[MAX_N] = { 0, };
+    memset(parent, -1, sizeof(parent));
+    cin >> N;
+    for (int i = 0; i < N - 1; ++i) {
+        cin >> A >> B;
+        parent[B] = A;
+    }
+    cin >> A >> B;
+
+    while (A != -1) {
+        check[A] = 1;
+        A = parent[A];
     }
 
-    //root찾으면서 level세팅
-    for(int i=0;i<N;++i){
-        if(parent[i]==-1) root=i;
-        setLevel(level,parent,i);
+    while (B != -1) {
+        if (check[B]) {
+            cout << B << endl;
+            return;
+        }
+        B = parent[B];
     }
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL); cout.tie(NULL);
-	freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    //freopen("input.txt", "r", stdin);
     int T;
-    cin>>T;
-    while(T--)
-	    solve();
-	return 0;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
 }
---
+++ study/2021-11-28/2_사촌.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	freopen("input.txt", "r", stdin);
+	solve();
+	return 0;
+}
