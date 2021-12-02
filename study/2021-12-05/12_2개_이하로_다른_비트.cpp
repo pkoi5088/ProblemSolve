6c39ec4830a8f21c0fed9ae422df35ea55cef086
pkoi5088 <pkoi5088@gmail.com>
2021-12-02 23:39:12 +0900

    2021-12-05

--- study/2021-12-05/12_2개_이하로_다른_비트.cpp
+++ study/2021-12-05/12_2개_이하로_다른_비트.cpp
@@ -9,10 +9,8 @@ vector<long long> solution(vector<long long> numbers) {
         if(number%2==0)
             answer.push_back(number+1);
         else{
-            long long xor1=number^(number+1);
-            xor1+=1;
-            xor1/=2;
-            answer.push_back((number|xor1)^(xor1/2));
+            long long x=(number^(number+1))+1;
+            answer.push_back(number+x/4);
         }
     }
     return answer;
