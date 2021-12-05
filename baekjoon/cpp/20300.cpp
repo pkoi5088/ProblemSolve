c23552f41ff4757499a9d89df22b4f73c975a3d5
pkoi5088 <pkoi5088@gmail.com>
2021-12-05 14:05:50 +0900

    2021-12-05

---
+++ baekjoon/cpp/20300.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 10000
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+    long long arr[MAX],ret=-1;
+    int N;
+    cin>>N;
+    for(int i=0;i<N;++i)
+        cin>>arr[i];
+    sort(arr,arr+N);
+    if(N%2){
+        ret=arr[N-1];
+        N-=1;
+    }
+    for(int i=0;i<N/2;++i){
+        ret=max(ret,arr[N-i-1]+arr[i]);
+    }
+    cout<<ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	//freopen("input.txt", "r", stdin);
+	solve();
+	return 0;
+}
