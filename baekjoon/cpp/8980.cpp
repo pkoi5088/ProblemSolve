d06832de0ae746b53471f250e637f6e0ca738cf2
Seoki <pkoi5088@gmail.com>
2021-12-25 19:26:37 +0900

    2021-12-25

--- baekjoon/cpp/8980.cpp
+++ baekjoon/cpp/8980.cpp
@@ -1,55 +1,46 @@
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
 #include <vector>
-#include <queue>
-#include <deque>
-#include <set>
-#include <map>
-#include <stack>
-#include <string>
-#define INF 10001
+#include <algorithm>
 #define endl '\n'
 
 using namespace std;
 
-struct Schedule {
-	int from, to, weight;
-};
+bool cmp(pair<pair<int, int>, int>p1, pair<pair<int, int>, int>p2) {
+	if (p1.first.second == p2.first.second)
+		return p1.first.first < p1.first.first;
+	return p1.first.second < p2.first.second;
+}
 
-bool operator<(Schedule& s1, Schedule& s2) {
-	if (s1.to == s2.to)
-		return s1.from < s2.from;
-	return s1.to < s2.to;
+int getMin(vector<int> capacity, int start, int end) {
+	int ret = capacity[start];
+	for (int i = start + 1; i < end; ++i) {
+		ret = min(ret, capacity[i]);
+	}
+	return ret;
 }
 
 void solve() {
-	vector<Schedule> village;
-	vector<int> weight;
-	int N, C, M, result = 0;
+	vector<pair<pair<int, int>, int>> v;
+	vector<int> capacity;
+	int N, M, C;
 	cin >> N >> C >> M;
-	weight.resize(N, 0);
+	v.resize(M);
+	capacity.resize(N + 1, C);
 	for (int i = 0; i < M; ++i) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		Schedule tmp;
-		tmp.from = a - 1; tmp.to = b - 1; tmp.weight = c;
-		village.push_back(tmp);
+		cin >> v[i].first.first >> v[i].first.second >> v[i].second;
 	}
-	sort(village.begin(), village.end());
+	sort(v.begin(), v.end(), cmp);
 
+	int ret = 0;
 	for (int i = 0; i < M; ++i) {
-		int from = village[i].from, to = village[i].to;
-		int rest = village[i].weight;
-		for (int j = from; j < to; ++j) {
-			rest = min(rest, C - weight[j]);
+		int start = v[i].first.first, end = v[i].first.second, box = v[i].second;
+		int c = min(box, getMin(capacity, start, end));
+		ret += c;
+		for (int i = start; i < end; ++i) {
+			capacity[i] -= c;
 		}
-		for (int j = from; j < to; ++j) {
-			weight[j] += rest;
-		}
-		result += rest;
 	}
-	cout << result;
+	cout << ret;
 }
 
 int main() {
