be38cc69250707fac111c733714923a5bc527e38
pkoi5088 <pkoi5088@gmail.com>
2022-01-09 09:41:33 +0900

    2022-01-09

--- study/2022-01-09/7_숫자의_표현.cpp
+++ study/2022-01-09/7_숫자의_표현.cpp
@@ -7,24 +7,18 @@ using namespace std;
     https://programmers.co.kr/learn/courses/30/lessons/12924
 */
 
-int getSum(int n){
-    return n*(n+1)/2;
-}
-
-int getSectionSum(int s,int e){
-    return getSum(e)-getSum(s-1);
-}
-
 int solution(int n) {
-    int answer = 0,start=1,end=1;
+    int answer = 0,start=1,end=1,t=1;
     while(start<=n){
-        int t=getSectionSum(start,end);
         if(t==n){
             answer+=1;
             end+=1;
+            t+=end;
         }else if(t<n){
             end+=1;
+            t+=end;
         }else{
+            t-=start;
             start+=1;
         }
     }
