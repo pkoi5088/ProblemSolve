6399ff24144b33c3ac6498d52ad640a3a5be56b4
pkoi5088 <pkoi5088@gmail.com>
2022-01-14 17:25:27 +0900

    2022-01-16

---
+++ study/2022-01-16/1_2xn_타일링.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define MAX 1001
+#define MOD 10007
+#define endl '\n'
+
+using namespace std;
+
+/*
+	https://www.acmicpc.net/problem/11726
+*/
+
+int dp[MAX];
+
+void solve() {
+    dp[0]=dp[1]=1;
+    int N;
+    cin>>N;
+    for(int i=2;i<=N;++i){
+        dp[i]=(dp[i-1]+dp[i-2])%MOD;
+    }
+    cout<<dp[N];
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	freopen("input.txt", "r", stdin);
+	solve();
+	return 0;
+}
