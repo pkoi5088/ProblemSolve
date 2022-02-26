d9ccc773433cd7daa395d7ceceb4fdca49ab17da
pkoi5088 <pkoi5088@gmail.com>
2022-02-26 16:38:31 +0900

    2022-02-27

---
+++ study/2022-02-27/2_수도배관공사.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#define MAX_P 350
+#define MAX_D 100001
+#define endl '\n'
+
+using namespace std;
+
+/*
+    https://www.acmicpc.net/problem/2073
+*/
+
+//arr.first: 길이, arr.second: 용량
+int dp[MAX_D];
+pair<int, int> arr[MAX_P];
+int D, P;
+
+void solve() {
+    memset(dp, 0, sizeof(dp));
+    cin >> D >> P;
+    for (int i = 0; i < P; ++i) {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    dp[0] = (1 << 30);
+
+    for (int i = 1; i < P; ++i) {
+        for (int j = D; j >= 0; --j) {
+            if (j - arr[i].first >= 0)
+                dp[j] = max(dp[j], min(dp[j - arr[i].first],arr[i].second));
+        }
+    }
+    cout << dp[D];
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    solve();
+    return 0;
+}
---
+++ study/2022-02-27/3_문자열_제거.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <cstring>
+#define MAX 1000
+#define endl '\n'
+
+using namespace std;
+
+/*
+    https://www.acmicpc.net/problem/21941
+*/
+
+map<string, int> table;
+string S;
+int M, dp[MAX];
+
+int getDP(int s) {
+    if (s >= S.size()) return 0;
+    int& ret = dp[s];
+    if (ret != -1) return ret;
+
+    string subStr;
+    ret = 0;
+    //s부터e까지 구간 탐색
+    for (int i = s; i < S.size(); ++i) {
+        subStr = S.substr(s, i - s + 1);
+        if (table.find(subStr) != table.end()) {
+            ret = max(ret, getDP(s + subStr.size()) + table[subStr]);
+        }
+        ret = max(ret, getDP(i + 1) + i - s + 1);
+    }
+    return ret;
+}
+
+void solve() {
+    memset(dp, -1, sizeof(dp));
+    cin >> S >> M;
+    for (int i = 0; i < M; ++i) {
+        string s;
+        int t;
+        cin >> s >> t;
+        table[s] = t;
+    }
+    cout << getDP(0);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    solve();
+    return 0;
+}
