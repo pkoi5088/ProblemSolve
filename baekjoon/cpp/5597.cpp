e77153c5bc68890acc103f1ff901b852402f507b
Seoki <pkoi5088@gmail.com>
2022-05-04 02:23:32 +0900

    2022-05-04

--- baekjoon/cpp/5597.cpp
+++ baekjoon/cpp/5597.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#define endl '\n'
 
 using namespace std;
 
+/*
+	https://www.acmicpc.net/problem/5597
+*/
+
+void solve() {
+    bool arr[31]={false,};
+    for(int i=0;i<28;++i){
+        int tmp;
+        cin>>tmp;
+        arr[tmp]=true;
+    }
+    for(int i=1;i<31;++i){
+        if(arr[i]==false) cout<<i<<endl;
+    }
+}
+
 int main() {
-	int n;
-	bool check[30];
-	for (int i = 0; i < 30; i++)
-		check[i] = false;
-
-	for (int i = 0; i < 28; i++) {
-		cin >> n;
-		check[n - 1] = true;
-	}
-
-	for (int i = 0; i < 30; i++) {
-		if (check[i] == false) {
-			cout << i + 1 << endl;
-		}
-	}
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	//freopen("input.txt", "r", stdin);
+	solve();
 	return 0;
 }
