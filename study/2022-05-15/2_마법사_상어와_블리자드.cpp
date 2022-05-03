59ae8976761ee95352848001eae4442a86dd2c7e
Seoki <pkoi5088@gmail.com>
2022-05-03 19:52:34 +0900

    2022-05-15

--- study/2022-05-15/2_마법사_상어와_블리자드.cpp
+++ study/2022-05-15/2_마법사_상어와_블리자드.cpp
@@ -14,17 +14,6 @@ vector<pair<int, int>> route;
 int dx[4] = { -1,0,1,0 }, dy[4] = { 0,1,0,-1 };
 int N, M;
 
-void print() {
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            cout << arr[i][j] << ' ';
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
 void init() {
     int x = 0, y = 0, dir = 1;
     while (x != N / 2 || y != N / 2) {
@@ -44,66 +33,77 @@ void init() {
     reverse(route.begin(), route.end());
 }
 
-void pulling() {
-    vector<vector<int>> tmp(N, vector<int>(N, 0));
-    vector<int> v;
-    for (int i = 0; i < route.size(); ++i) {
-        int x = route[i].first, y = route[i].second;
-        if (arr[x][y] != 0) {
-            v.push_back(arr[x][y]);
-        }
-    }
-
-    for (int i = 0; i < v.size(); ++i) {
-        int x = route[i].first, y = route[i].second;
-        tmp[x][y] = v[i];
-    }
-    arr = tmp;
-}
-
 void explosion(int& ret) {
     //stack용 vector first: 숫자, second: 개수
-    vector<vector<int>> tmp(N, vector<int>(N, 0));
+    vector<vector<int>> tmp;
     vector<pair<int, int>> v;
+    //flag: 폭발이 일어났는가
+    bool flag;
+
+    //폭발이 일어나지 않을때까지 반복
+    do{
+        flag=false;
+        tmp.clear(); v.clear();
+        tmp.resize(N,vector<int>(N,0));
+
+        for(int i=0;i<route.size();++i){
+            int x=route[i].first,y=route[i].second;
+            if(arr[x][y]==0) continue;
+
+            if(v.empty()){
+                v.push_back({arr[x][y],1});
+            }else if(v.back().first==arr[x][y]){
+                int idx=v.size()-1;
+                v[idx].second+=1;
+            }else if(v.back().first!=arr[x][y]){
+                //폭발
+                if(v.back().second>=4){
+                    ret+=v.back().first*v.back().second;
+                    v.pop_back();
+                    flag=true;
+                }
+                v.push_back({arr[x][y],1});
+            }
+        }
+        if(!v.empty()&&v.back().second>=4){
+            ret+=v.back().first*v.back().second;
+            v.pop_back();
+            flag=true;
+        }
+
+
+        for (int i = 0, idx = 0; i < v.size() && idx < route.size(); ++i) {
+            int num=v[i].first,c=v[i].second;
+            for(int j=0;j<c&&idx<route.size();++j,++idx){
+                int x=route[idx].first,y=route[idx].second;
+                tmp[x][y]=num;
+            }
+        }
+        arr=tmp;
+    }while(flag);
+}
+
+void replace() {
+    vector<vector<int>> tmp(N, vector<int>(N, 0));
+    vector<pair<int,int>> v;
+    //누적
     for (int i = 0; i < route.size(); ++i) {
         int x = route[i].first, y = route[i].second;
         if (arr[x][y] != 0) {
-            if (v.empty()) {
-                v.push_back({ arr[x][y],1 });
-            }
-            //스택 카운트 증가
-            else if (v.back().first == arr[x][y]) {
-                int idx = v.size() - 1;
-                v[idx].second += 1;
-            }
-            //폭발 연산
-            else if (v.back().first != arr[x][y]) {
-                //폭발
-                if (v.back().second >= 4) {
-                    ret += v.back().first * v.back().second;
-                    v.pop_back();
-                }
-
-                if (!v.empty() && v.back().first == arr[x][y]) {
-                    int idx = v.size() - 1;
-                    v[idx].second += 1;
-                }
-                else {
-                    v.push_back({ arr[x][y],1 });
-                }
+            if(v.empty()||v.back().first!=arr[x][y]){
+                v.push_back({arr[x][y],1});
+            }else{
+                int idx=v.size()-1;
+                v[idx].second+=1;
             }
         }
     }
-    if (!v.empty() && v.back().second >= 4) {
-        ret += v.back().first * v.back().second;
-        v.pop_back();
-    }
 
-    for (int i = 0, idx = 0; i < v.size() && idx < route.size(); ++i,idx+=2) {
-        int x = route[idx].first, y = route[idx].second;
-        int nx = route[idx + 1].first, ny = route[idx + 1].second;
-        tmp[x][y] = v[i].second;
-        tmp[nx][ny] = v[i].first;
+    for (int i = 0,idx=0; i < v.size()&&idx<route.size(); ++i,idx+=2) {
+        int x=route[idx].first,y=route[idx].second;
+        int nx=route[idx+1].first,ny=route[idx+1].second;
+        tmp[x][y]=v[i].second;
+        tmp[nx][ny]=v[i].first;
     }
     arr = tmp;
 }
@@ -132,11 +132,9 @@ void solve() {
             arr[nx][ny] = 0;
         }
 
-        print();
         //폭발 후 당기기
         explosion(ret);
-        //pulling();
-        print();
+        replace();
     }
     cout << ret;
 }
@@ -144,7 +142,7 @@ void solve() {
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
-    freopen("input.txt", "r", stdin);
+    //freopen("input.txt", "r", stdin);
     solve();
     return 0;
 }
---
+++ study/2022-05-15/4_8진수_2진수.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+
+using namespace std;
+
+/*
+	https://www.acmicpc.net/problem/1212
+*/
+
+void solve() {
+    vector<string> v={"000","100","010","110","001","101","011","111"};
+    string s,ret;
+    cin>>s;
+    if(s=="0") cout<<s;
+    else{
+        for(int i=s.size()-1;i>=0;--i){
+            ret+=v[s[i]-'0'];
+        }
+        while(ret.back()=='0')
+            ret.pop_back();
+        reverse(ret.begin(),ret.end());
+        cout<<ret;
+    }
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	freopen("input.txt", "r", stdin);
+	solve();
+	return 0;
+}
