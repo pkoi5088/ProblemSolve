3902ab1743edda251f96f5b8e30c97fca5fc02f3
pkoi5088 <pkoi5088@gmail.com>
2022-05-22 10:16:17 +0900

    2022-05-22

--- study/2022-05-15/4_8진수_2진수.cpp
+++ study/2022-05-15/4_8진수_2진수.cpp
@@ -29,7 +29,7 @@ void solve() {
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL); cout.tie(NULL);
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 	solve();
 	return 0;
 }
