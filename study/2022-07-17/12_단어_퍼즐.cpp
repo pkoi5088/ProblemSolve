c97571e459bcaa254fc505eec4e2ba4d45a4d70a
pkoi5088 <pkoi5088@gmail.com>
2022-07-31 11:02:30 +0900

    2022-07-24

--- study/2022-07-17/12_단어_퍼즐.cpp
+++ study/2022-07-17/12_단어_퍼즐.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#define INF 3000000
 #define MAX 20000
+
 using namespace std;
 
-/*
-    https://school.programmers.co.kr/learn/courses/30/lessons/12983
-*/ 
-
-int N;
-//dp[i]: t[i:]에서 만들수 있는 경우의수
-int dp[MAX];
-
-int min(int a, int b) {
-    if (a == -2) return b;
-    if (b == -2) return a;
-    return a < b ? a : b;
+int min(int a,int b){
+    if(a==-1) return b;
+    if(b==-1) return a;
+    return a<b?a:b;
 }
 
-//p가 t안에 존재하는가
-bool check(string t, int length, string p) {
-    if (p.size() > t.size() - length) return false;
-    for (int i = 0; i < p.size(); ++i) {
-        if (p[i] != t[i + length]) return false;
+//dp[i]: t[i:]에서 만들수있는 단어조각의 최소값
+int dp[MAX];
+
+void init(){
+    for(int i=0;i<MAX;++i){
+        dp[i]=INF;
+    }
+}
+
+bool check(int idx,string t,string p){
+    for(int i=0;i<p.size();++i){
+        if(idx+i>=t.size()||p[i]!=t[idx+i]) return false;
     }
     return true;
 }
 
-int getDP(int idx, vector<string>& strs,string& t) {
-    if (idx >= N) return 0;
-    int& ret = dp[idx];
-    if (ret != -2) return ret;
-
-    for (int i = 0; i < strs.size(); ++i) {
-        if (check(t, idx, strs[i])) {
-            int tmp = getDP(idx + strs[i].size(), strs, t);
-            if (tmp != -2) {
-                ret = min(ret, tmp + 1);
+int getDP(int idx,vector<string>& strs, string& t){
+    if(idx>=t.size()) return 0;
+    int& ret=dp[idx];
+    if(ret!=INF) return ret;
+    
+    ret=-1;
+    for(int i=0;i<strs.size();++i){
+        if(check(idx,t,strs[i])){
+            int tmp=getDP(idx+strs[i].size(),strs,t);
+            if(tmp!=-1){
+                ret=min(ret,tmp+1);
             }
         }
     }
@@ -45,12 +47,10 @@ int getDP(int idx, vector<string>& strs,string& t) {
 
 int solution(vector<string> strs, string t)
 {
-    int answer = 0;
-    N = t.size();
-    for (int i = 0; i < N; ++i) {
-        dp[i] = -2;
-    }
-    answer = getDP(0, strs, t);
-    if (answer == -2) return -1;
+    init();
+    int answer = getDP(0,strs,t);
+    
+    
+
     return answer;
 }
