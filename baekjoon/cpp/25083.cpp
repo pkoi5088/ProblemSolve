e9afdaf46f4972fee8f6e2e05761147cb1f6e072
Seoki <44914802+pkoi5088@users.noreply.github.com>
2023-06-16 00:37:44 +0900

    Create 25083.cpp
    2023-06-16

---
+++ baekjoon/cpp/25083.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define endl '\n'
+
+using namespace std;
+
+void solve() {
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r\'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+	//freopen("input.txt", "r", stdin);
+	solve();
+	return 0;
+}
