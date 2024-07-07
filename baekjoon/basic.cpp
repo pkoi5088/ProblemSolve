3dc3ba9176306e8d7c967f7f4506338c9dc1e5cb
Seoki <pkoi5088@gmail.com>
2024-07-07 22:49:46 +0900

    baekjoon/basic.cpp 수정 - 매크로 적용

--- baekjoon/basic.cpp
+++ baekjoon/basic.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #define endl '\n'
 
+#define INPUT_TESTCASE	\
+	int T;				\
+	cin>>T;				\
+	while(T--)			\
+		solve();		
+#define INPUT solve();
+
 using namespace std;
 
 void solve() {
-    
+
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL); cout.tie(NULL);
 	freopen("input.txt", "r", stdin);
-	solve();
+	INPUT;
 	return 0;
 }
